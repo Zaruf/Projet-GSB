86fb460fc083b32b86d55c9d04be23daaf573163
Vincent <vincent.douchet13@gmail.com>
2020-03-13 08:51:44 +0100

    efhfg

---
+++ GSB/GSB/AgenceView.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace GSB
+{
+    partial class AgenceView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgAgences = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgAgences)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgAgences
+            // 
+            this.dgAgences.AllowUserToAddRows = false;
+            this.dgAgences.AllowUserToDeleteRows = false;
+            this.dgAgences.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dgAgences.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgAgences.Location = new System.Drawing.Point(26, 12);
+            this.dgAgences.Name = "dgAgences";
+            this.dgAgences.ReadOnly = true;
+            this.dgAgences.Size = new System.Drawing.Size(548, 314);
+            this.dgAgences.TabIndex = 2;
+            // 
+            // AgenceView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 485);
+            this.Controls.Add(this.dgAgences);
+            this.Name = "AgenceView";
+            this.Text = "AgenceView";
+            this.Load += new System.EventHandler(this.AgenceView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgAgences)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgAgences;
+
+
+    }
+}
---
+++ GSB/GSB/AgenceView.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GSB
+{
+    public partial class AgenceView : Form
+    {
+        public AgenceView()
+        {
+            InitializeComponent();
+        }
+
+        // on déclare un objet de la classe LienBdd
+        internal LienBdd uneCn;
+        private DataTable dt = new DataTable();
+        private int i = 0;
+        private int noLigne;
+        private void AgenceView_Load(object sender, EventArgs e)
+        {
+            // connection à la bdd hippo sous sqlserver
+            uneCn = new LienBdd();
+            try
+            {
+                // remplissage de la data grid view
+                dt = uneCn.ObtenirAgences();  // on appelle la méthode ObtenirCourses de la classe LienBdd
+                this.dgAgences.DataSource = dt;
+                this.dgAgences.DataMember = dt.TableName;
+                this.dgAgences.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        
+
+
+    }
+}
